d8b5563faf37c216b6436839e2d393b586c7ff4d
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-10 20:56:55 -0700

    Added styling to button

--- src/bin2dec.jsx
+++ src/bin2dec.jsx
@@ -10,21 +10,43 @@ class Bin2Dec extends React.Component {
       decimalValue: null
     }
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
   }
 
+  handleChange(event) {
+    if (event.target.getAttribute('id') === 'binaryValue') {
+      this.setState({
+        binaryValue: event.target.value
+      })
+    }
+  }
   render() {
     return (
-      <div className="row">
-        <div className="col-6">
-          <Form>
-            <Form.Label htmlFor="binaryValue" className="mb-3">Binary</Form.Label>
-            <Form.Control required type="number" placeholder="Enter binary value" id="binaryValue" />
-            <Button type="submit">Convert</Button>
-          </Form>
+      <div className="container-fluid">
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="col-6">
+            <Form>
+              <div className="row mb-3">
+                <Form.Label htmlFor="binaryValue" className="col-4 col-form-label text-end">BINARY</Form.Label>
+                <div className="col-8">
+                  <Form.Control required type="number" value={this.binaryValue} placeholder="Enter value" id="binaryValue" onChange={this.handleChange} />
+                </div>
+              </div>
+              <div className="row mb-3">
+                <Form.Label htmlFor="decimalValue" className="col-4 col-form-label">DECIMAL</Form.Label>
+                <div className="col-8">
+                  <Form.Control readOnly type="number" value={this.decimalValue} id="decimalValue" />
+                </div>
+              </div>
+              <div className="text-end">
+                <Button type="submit">Convert</Button>
+              </div>
+            </Form>
+          </div>
         </div>
       </div>
     )

7835fe7a5eb1a97686550d6968dbef83f2a13098
Eleonora Hwang <eleonorahwang@gmail.com>
2022-05-10 17:43:03 -0700

    Created Bin2Dec class component

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import react from 'react';
+import reactDom from 'react-dom';
+import Bin2Dec from './bin2dec';
+
+const root = ReactDomClient.createRoot(document.querySelector('#root'));
+root.render(<Bin2Dec />);
